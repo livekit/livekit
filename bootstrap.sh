32daa257bb51a279ce9b288ca1b8bdf8a3a61cb4
hn8 <10730886+hn8@users.noreply.github.com>
2021-08-31 02:35:03 +0800

    cleanup protoc (#100)
    https://github.com/livekit/livekit-server/pull/95

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if ! command -v protoc &> /dev/null
-then
-  echo "protoc is required and not found. please install"
-  exit 1
-fi
-
 if ! command -v mage &> /dev/null
 then
   pushd /tmp
@@ -23,17 +17,3 @@ then
 fi
 
 go mod download
-
-GO_VERSION=`go version | { read _ _ v _; echo ${v#go}; }`
-GO_TARGET_VERSION=1.17
-
-function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
-
-if [ $(version $GO_VERSION) -ge $(version $GO_TARGET_VERSION) ];
-  then
-    go install github.com/twitchtv/twirp/protoc-gen-twirp@v8.1.0
-    go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.27.1
-  else
-    go get -u github.com/twitchtv/twirp/protoc-gen-twirp@v8.1.0
-    go get -u google.golang.org/protobuf/cmd/protoc-gen-go@v1.27.1
-fi
