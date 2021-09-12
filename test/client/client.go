7a0c4a5c3619b3d55a2c356a78f3e6ec4f216505
David Zhao <david@davidzhao.com>
2021-09-12 00:16:12 -0700

    longer publisher connect timeout

--- test/client/client.go
+++ test/client/client.go
@@ -551,7 +551,7 @@ func (c *RTCClient) ensurePublisherConnected() error {
 	}
 
 	// wait until connected
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	for {
 		select {
