68cf00fa18013601dcd21a368cfb81436dc15e37
Artur Shellunts <shellunts.artur@gmail.com>
2021-10-03 11:02:07 +0200

    Refactor getConfigString

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -208,15 +208,15 @@ func startServer(c *cli.Context) error {
 	return server.Start()
 }
 
-func getConfigString(configFile string, configBody string) (string, error) {
-	if configBody == "" {
-		if configFile != "" {
-			content, err := ioutil.ReadFile(configFile)
-			if err != nil {
-				return "", err
-			}
-			configBody = string(content)
-		}
+func getConfigString(configFile string, inConfigBody string) (string, error) {
+	if inConfigBody != "" || configFile == "" {
+		return inConfigBody, nil
 	}
-	return configBody, nil
+
+	outConfigBody, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return "", err
+	}
+
+	return string(outConfigBody), nil
 }
