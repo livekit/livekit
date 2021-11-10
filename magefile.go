0a769662aabe866da273979953f393aa6739781d
David Zhao <david@davidzhao.com>
2021-11-10 15:07:49 -0800

    Fixed multi-platform builds (#178)

--- magefile.go
+++ magefile.go
@@ -95,27 +95,18 @@ func BuildLinux() error {
 	return nil
 }
 
-// builds docker image for LiveKit server
-func Docker() error {
-	mg.Deps(generateWire)
-	cmd := exec.Command("docker", "build", ".", "-t", fmt.Sprintf("%s:v%s", imageName, version.Version))
-	connectStd(cmd)
-	if err := cmd.Run(); err != nil {
-		return err
-	}
-	return nil
-}
-
+// builds and publish snapshot docker image
 func PublishDocker() error {
-	mg.Deps(Docker)
-
 	// don't publish snapshot versions as latest or minor version
 	if !strings.Contains(version.Version, "SNAPSHOT") {
 		return errors.New("Cannot publish non-snapshot versions")
 	}
 
 	versionImg := fmt.Sprintf("%s:v%s", imageName, version.Version)
-	cmd := exec.Command("docker", "push", versionImg)
+	cmd := exec.Command("docker", "buildx", "build",
+		"--push", "--platform", "linux/amd64,linux/arm64",
+		"--tag", versionImg,
+		".")
 	connectStd(cmd)
 	if err := cmd.Run(); err != nil {
 		return err
