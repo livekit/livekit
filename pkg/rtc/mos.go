4be0ae7cd6f8c5736d3f1605bd99b79a3ae297a0
shishir gowda <shishir@livekit.io>
2021-12-15 11:09:18 -0500

    Account for delay while calculating scores
    Signed-off-by: shishir gowda <shishir@livekit.io>

--- pkg/rtc/mos.go
+++ pkg/rtc/mos.go
@@ -40,7 +40,7 @@ func mosAudioEmodel(cur, prev *ConnectionStat) float64 {
 	rx := 93.2 - float64(percentageLost)
 	ry := 0.18*rx*rx - 27.9*rx + 1126.62
 
-	d := float64(rtt + cur.Jitter)
+	d := float64(rtt + (cur.Delay / 1000) + cur.Jitter)
 	h := d - 177.3
 	if h < 0 {
 		h = 0
