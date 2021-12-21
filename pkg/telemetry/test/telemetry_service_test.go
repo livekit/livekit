d0f9003a0312c49c6c92a602d79e9ea317964585
Artur Shellunts <shellunts.artur@gmail.com>
2021-12-21 19:50:36 +0100

    Add Test_AnalyticsSentWhenParticipantLeaves

--- pkg/telemetry/test/telemetry_service_test.go
+++ pkg/telemetry/test/telemetry_service_test.go
@@ -26,24 +26,43 @@ func createFixture() *telemetryServiceFixture {
 func Test_OnDownstreamPacket(t *testing.T) {
 	fixture := createFixture()
 
+	//prepare
 	room := &livekit.Room{}
 	partSID := "part1"
 	clientInfo := &livekit.ClientInfo{Sdk: 2}
 	participantInfo := &livekit.ParticipantInfo{Sid: partSID}
 	fixture.sut.ParticipantJoined(context.Background(), room, participantInfo, clientInfo)
+
+	//do
 	packets := []int{33, 23}
 	totalBytes := packets[0] + packets[1]
 	totalPackets := len(packets)
 	for i := range packets {
 		fixture.sut.OnDownstreamPacket(partSID, packets[i])
 	}
-
 	fixture.sut.SendAnalytics()
 
+	//test
 	require.Equal(t, 1, fixture.analytics.SendStatsCallCount())
 	_, stats := fixture.analytics.SendStatsArgsForCall(0)
 	require.Equal(t, 1, len(stats))
 	require.Equal(t, livekit.StreamType_DOWNSTREAM, stats[0].Kind)
 	require.Equal(t, totalBytes, int(stats[0].TotalBytes))
 	require.Equal(t, totalPackets, int(stats[0].TotalPackets))
+}
+
+func Test_AnalyticsSentWhenParticipantLeaves(t *testing.T) {
+	fixture := createFixture()
+
+	//prepare
+	room := &livekit.Room{}
+	partSID := "part1"
+	participantInfo := &livekit.ParticipantInfo{Sid: partSID}
+	fixture.sut.ParticipantJoined(context.Background(), room, participantInfo)
+
+	//do
+	fixture.sut.ParticipantLeft(context.Background(), room, participantInfo)
+
+	//test
+	require.Equal(t, 1, fixture.analytics.SendStatsCallCount())
 }
