d319384936a068d0a606eb6a6a0a85fc2cfe277e
cnderrauber <zengjie9004@gmail.com>
2022-01-09 23:03:58 +0800

    fix test case

--- test/client/client.go
+++ test/client/client.go
@@ -126,6 +126,7 @@ func NewRTCClient(conn *websocket.Conn) (*RTCClient, error) {
 	conf := rtc.WebRTCConfig{
 		Configuration: rtcConf,
 	}
+	conf.SettingEngine.SetLite(false)
 	codecs := []*livekit.Codec{
 		{
 			Mime: "audio/opus",
