202b8501244e8aa7199d700c66fd6290f8f7dc15
shishir gowda <shishir@livekit.io>
2022-02-07 14:31:25 -0500

    Add ParticipantActive telemetry method
    Signed-off-by: shishir gowda <shishir@livekit.io>

--- pkg/telemetry/test/telemetry_service_events_test.go
+++ pkg/telemetry/test/telemetry_service_events_test.go
@@ -109,3 +109,50 @@ func Test_OnTrackUpdate_EventIsSent(t *testing.T) {
 	require.Equal(t, height, event.Track.Height)
 
 }
+
+func Test_OnParticipantActive_EventIsSent(t *testing.T) {
+	fixture := createFixture()
+
+	// prepare participant to change status
+	room := &livekit.Room{Sid: "RoomSid", Name: "RoomName"}
+	partSID := "part1"
+
+	clientInfo := &livekit.ClientInfo{
+		Sdk:            2,
+		Version:        "v1",
+		Os:             "mac",
+		OsVersion:      "v1",
+		DeviceModel:    "DM1",
+		Browser:        "chrome",
+		BrowserVersion: "97.0.1",
+	}
+	clientMeta := &livekit.AnalyticsClientMeta{
+		Region:     "dark-side",
+		Node:       "moon",
+		ClientAddr: "127.0.0.1",
+	}
+	participantInfo := &livekit.ParticipantInfo{Sid: partSID}
+
+	// do
+	fixture.sut.ParticipantJoined(context.Background(), room, participantInfo, clientInfo, clientMeta)
+
+	// test
+	require.Equal(t, 1, fixture.analytics.SendEventCallCount())
+	_, event := fixture.analytics.SendEventArgsForCall(0)
+
+	// test
+	// do
+	clientMetaConnect := &livekit.AnalyticsClientMeta{
+		ClientConnectTime: 420,
+	}
+	fixture.sut.ParticipantActive(context.Background(), livekit.ParticipantID(partSID), clientMetaConnect)
+
+	require.Equal(t, 2, fixture.analytics.SendEventCallCount())
+	_, eventActive := fixture.analytics.SendEventArgsForCall(1)
+	require.Equal(t, livekit.AnalyticsEventType_PARTICIPANT_ACTIVE, eventActive.Type)
+	require.Equal(t, partSID, eventActive.ParticipantId)
+	require.Equal(t, room.Sid, eventActive.RoomId)
+	require.Equal(t, room, event.Room)
+
+	require.Equal(t, clientMeta.ClientConnectTime, event.ClientMeta.ClientConnectTime)
+}
