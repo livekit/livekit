f89752c4f4a32061b9d2a6f36f87d985b4f24c28
David Colburn <xero73@gmail.com>
2022-03-14 16:30:27 -0700

    use egress results channel

--- pkg/service/errors.go
+++ pkg/service/errors.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrEgressNotFound       = errors.New("egress does not exist")
+	ErrEgressNotConnected   = errors.New("egress not connected (redis required)")
 	ErrRoomNotFound         = errors.New("requested room does not exist")
 	ErrRoomLockFailed       = errors.New("could not lock room")
 	ErrRoomUnlockFailed     = errors.New("could not unlock room, lock token does not match")
