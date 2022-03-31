88db48ee0561c457f2de20e977a309d187360e50
David Zhao <david@davidzhao.com>
2022-03-31 12:20:05 -0700

    Use type-specific track prefixes.
    Making it easier to identify track type from its ID

--- pkg/rtc/participant.go
+++ pkg/rtc/participant.go
@@ -1238,10 +1238,17 @@ func (p *ParticipantImpl) addPendingTrack(req *livekit.AddTrackRequest) *livekit
 		return nil
 	}
 
+	trackPrefix := utils.TrackPrefix
+	if req.Type == livekit.TrackType_VIDEO {
+		trackPrefix += "V"
+	} else if req.Type == livekit.TrackType_AUDIO {
+		trackPrefix += "A"
+	}
+
 	ti := &livekit.TrackInfo{
 		Type:       req.Type,
 		Name:       req.Name,
-		Sid:        utils.NewGuid(utils.TrackPrefix),
+		Sid:        utils.NewGuid(trackPrefix),
 		Width:      req.Width,
 		Height:     req.Height,
 		Muted:      req.Muted,
