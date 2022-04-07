6968811507c48453d016ac6046e3e9b6a6510c4c
http418 <http418@163.com>
2022-04-07 23:42:56 +0800

    provide interface for token

--- pkg/service/server.go
+++ pkg/service/server.go
@@ -97,6 +97,11 @@ func NewLivekitServer(conf *config.Config,
 	mux.Handle("/rtc", rtcService)
 	mux.HandleFunc("/rtc/validate", rtcService.Validate)
 	mux.HandleFunc("/", s.healthCheck)
+	mux.HandleFunc("/internal/token", s.internalToken)
+	if conf.Development {
+		mux.HandleFunc("/debug/goroutine", s.debugGoroutines)
+		mux.HandleFunc("/debug/rooms", s.debugInfo)
+	}
 
 	s.httpServer = &http.Server{
 		Addr:    fmt.Sprintf(":%d", conf.Port),
@@ -301,6 +306,57 @@ func (s *LivekitServer) backgroundWorker() {
 		case <-roomTicker.C:
 			s.roomManager.CloseIdleRooms()
 		}
+	}
+}
+
+type RequestInternalToken struct {
+	Method       string `json:"kind,omitempty"`
+	CallKey      string `json:"call_key,omitempty"`
+	NameCalled   string `json:"name_called,omitempty"`
+	NameIdentity string `json:"name_identity,omitempty"`
+
+	InternalKey    string `json:"key"`
+	InternalSecret string `json:"secret"`
+}
+
+type ResultInternalToken struct {
+	Location string `json:"location,omitempty"`
+	Token    string `json:"token,omitempty"`
+}
+
+func (s *LivekitServer) internalToken(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var req RequestInternalToken
+	if err := decoder.Decode(&req); nil != err {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	var grant auth.VideoGrant
+	at := auth.NewAccessToken(req.InternalKey, req.InternalSecret)
+	switch req.Method {
+	case "start":
+		grant.RoomCreate = true
+		at.AddGrant(&grant)
+	case "invite":
+		grant.RoomJoin = true
+		grant.Room = req.CallKey
+		at.AddGrant(&grant).SetIdentity(req.NameIdentity).SetName(req.NameCalled).SetMetadata("metadata" + req.NameIdentity)
+	}
+	t, err := at.ToJWT()
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	result := ResultInternalToken{}
+	result.Location = "wss"
+	result.Token = t
+	if bytes, err := json.Marshal(result); nil != err {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Add("Content-Type", "application/json")
+		_, _ = w.Write(bytes)
 	}
 }
 
