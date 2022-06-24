0b630e15b646be8dce6b5cd6770f83f40a02e82d
cnderrauber <zengjie9004@gmail.com>
2022-06-24 15:06:07 +0800

    disable ice lite by default (#784)
    * disable ice lite by default

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -195,7 +195,6 @@ func NewConfig(confString string, c *cli.Context) (*Config, error) {
 		Port: 7880,
 		RTC: RTCConfig{
 			UseExternalIP:     false,
-			UseICELite:        true,
 			TCPPort:           7881,
 			UDPPort:           0,
 			ICEPortRangeStart: 0,
