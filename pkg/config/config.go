49cf15cdca53a17d6dbd422b5cd963efd947afd2
Raja Subramanian <raja.gobi@tutanota.com>
2022-08-10 10:42:46 +0530

    Fix filtering candidates  (#901)
    * Filter candidate after setting description
    
    * comment
    
    * Fix filtering candidates
    
    - For offer/answer from remote, do filtering before setting remote
      description so that Pion does not see filtered candidates
    - For offer/answer originating from server, do filtering after setting
      local description (comments in code) so that remote side does not
      see filtered candidates.
    - Make logging a little consistent and use right context.
    
    * Comment
    
    * TCP fallback config and UT (broken now)
    
    * log SDP only when preferring TCP
    
    * Remove TCP fallback test attempt

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -38,7 +38,7 @@ type Config struct {
 	Video          VideoConfig        `yaml:"video,omitempty"`
 	Room           RoomConfig         `yaml:"room,omitempty"`
 	TURN           TURNConfig         `yaml:"turn,omitempty"`
-	Ingress        IngressConfig      `yaml:ingress,omitempty"`
+	Ingress        IngressConfig      `yaml:"ingress,omitempty"`
 	WebHook        WebHookConfig      `yaml:"webhook,omitempty"`
 	NodeSelector   NodeSelectorConfig `yaml:"node_selector,omitempty"`
 	KeyFile        string             `yaml:"key_file,omitempty"`
@@ -74,6 +74,9 @@ type RTCConfig struct {
 
 	// for testing, disable UDP
 	ForceTCP bool `yaml:"force_tcp,omitempty"`
+
+	// allow TCP fallback
+	AllowTCPFallback bool `yaml:"allow_tcp_fallback,omitempty"`
 }
 
 type TURNServer struct {
