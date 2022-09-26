932af81f346d0fb05d6ef5848d64d6f37d3488f0
Benjamin Pracht <benjamin@livekit.io>
2022-09-26 11:16:27 -0700

    Update strored version of an active ingress if no ingress server responds (#1031)
    This allows deleting and updating an ingress even if the ingress server that was handling it died. It does however mean that if the ingress responds again later, its state will be inconsistent. To somewhat make this less likely, also keep trying contacting the ingress for 1 min in the background.
    
    Also fixing a race where an active deleted Ingress would get recreated on delete because of the update triggered by the ingress session shutdown

--- pkg/service/ingress.go
+++ pkg/service/ingress.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
 
 	"google.golang.org/protobuf/proto"
 
@@ -13,6 +14,11 @@ import (
 	"github.com/livekit/protocol/livekit"
 	"github.com/livekit/protocol/logger"
 	"github.com/livekit/protocol/utils"
+)
+
+var (
+	initialTimeout = time.Second * 3
+	retryTimeout   = time.Minute * 1
 )
 
 type IngressService struct {
@@ -93,6 +99,41 @@ func (s *IngressService) CreateIngressWithUrlPrefix(ctx context.Context, urlPref
 	return info, nil
 }
 
+func (s *IngressService) sendRPCWithRetry(ctx context.Context, req *livekit.IngressRequest) (*livekit.IngressInfo, error) {
+	type result struct {
+		info *livekit.IngressInfo
+		err  error
+	}
+
+	resChan := make(chan result, 1)
+
+	go func() {
+		cctx, _ := context.WithTimeout(context.Background(), retryTimeout)
+
+		for {
+			select {
+			case <-cctx.Done():
+				resChan <- result{nil, ingress.ErrNoResponse}
+				return
+			default:
+			}
+
+			i, err := s.rpcClient.SendRequest(cctx, req)
+			if err != ingress.ErrNoResponse {
+				resChan <- result{i, err}
+				return
+			}
+		}
+	}()
+
+	select {
+	case res := <-resChan:
+		return res.info, res.err
+	case <-time.After(initialTimeout):
+		return nil, ingress.ErrNoResponse
+	}
+}
+
 func (s *IngressService) UpdateIngress(ctx context.Context, req *livekit.UpdateIngressRequest) (*livekit.IngressInfo, error) {
 	roomName, err := EnsureJoinPermission(ctx)
 	if err != nil {
@@ -139,13 +180,14 @@ func (s *IngressService) UpdateIngress(ctx context.Context, req *livekit.UpdateI
 
 	case livekit.IngressState_ENDPOINT_BUFFERING,
 		livekit.IngressState_ENDPOINT_PUBLISHING:
-		info, err = s.rpcClient.SendRequest(ctx, &livekit.IngressRequest{
+		i, err := s.sendRPCWithRetry(ctx, &livekit.IngressRequest{
 			IngressId: req.IngressId,
 			Request:   &livekit.IngressRequest_Update{Update: req},
 		})
 		if err != nil {
-			logger.Errorw("could not update active ingress", err)
-			return nil, err
+			logger.Warnw("could not update active ingress", err)
+		} else {
+			info = i
 		}
 	}
 
@@ -193,13 +235,14 @@ func (s *IngressService) DeleteIngress(ctx context.Context, req *livekit.DeleteI
 	switch info.State.Status {
 	case livekit.IngressState_ENDPOINT_BUFFERING,
 		livekit.IngressState_ENDPOINT_PUBLISHING:
-		info, err = s.rpcClient.SendRequest(ctx, &livekit.IngressRequest{
+		i, err := s.sendRPCWithRetry(ctx, &livekit.IngressRequest{
 			IngressId: req.IngressId,
 			Request:   &livekit.IngressRequest_Delete{Delete: req},
 		})
 		if err != nil {
-			logger.Errorw("could not stop active ingress", err)
-			return nil, err
+			logger.Warnw("could not stop active ingress", err)
+		} else {
+			info = i
 		}
 	}
 
@@ -235,7 +278,7 @@ func (s *IngressService) updateWorker() {
 			// save updated info to store
 			err = s.store.UpdateIngress(context.Background(), res)
 			if err != nil {
-				logger.Errorw("could not update egress", err)
+				logger.Errorw("could not update ingress", err)
 			}
 
 		case <-s.shutdown:
