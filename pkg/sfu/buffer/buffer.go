882d6997cd9958a9d9e25c4489509a4f57fcab1d
boks1971 <raja.gobi@tutanota.com>
2022-10-23 09:33:17 +0530

    Do not log duplicate packet error.
    Also, use bucket.Err* as it has been moved to mediatransportutil

--- pkg/sfu/buffer/buffer.go
+++ pkg/sfu/buffer/buffer.go
@@ -243,7 +243,7 @@ func (b *Buffer) Read(buff []byte) (n int, err error) {
 		b.Lock()
 		if b.pPackets != nil && len(b.pPackets) > b.lastPacketRead {
 			if len(buff) < len(b.pPackets[b.lastPacketRead].packet) {
-				err = ErrBufferTooSmall
+				err = bucket.ErrBufferTooSmall
 				b.Unlock()
 				return
 			}
@@ -364,7 +364,7 @@ func (b *Buffer) calc(pkt []byte, arrivalTime int64) {
 			b.processHeaderExtensions(&rtpPacket, arrivalTime)
 		}
 
-		if err != ErrRTXPacket {
+		if err != bucket.ErrRTXPacket {
 			b.logger.Warnw("could not add RTP packet to bucket", err)
 		}
 		return
