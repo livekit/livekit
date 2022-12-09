a41018bf04681f191428896b02b12c3b49c4f00d
boks1971 <raja.gobi@tutanota.com>
2022-12-09 18:41:36 +0530

    base62 encode user provided name/identity.
    To ensure there are no clashes with internal delimiter while generating
    keys.

---
+++ pkg/routing/utils_test.go
@@ -0,0 +1,49 @@
+package routing
+
+import (
+	"testing"
+
+	"github.com/livekit/protocol/livekit"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUtils_ParticipantKey(t *testing.T) {
+	// encode/decode empty
+	encoded := participantKey("", "")
+	roomName, identity, err := parseParticipantKey(encoded)
+	require.NoError(t, err)
+	require.Equal(t, livekit.RoomName(""), roomName)
+	require.Equal(t, livekit.ParticipantIdentity(""), identity)
+
+	// decode invalid
+	roomName, identity, err = parseParticipantKey("abcd")
+	require.Error(t, err)
+
+	// encode/decode without delimiter
+	encoded = participantKey("room1", "identity1")
+	roomName, identity, err = parseParticipantKey(encoded)
+	require.NoError(t, err)
+	require.Equal(t, livekit.RoomName("room1"), roomName)
+	require.Equal(t, livekit.ParticipantIdentity("identity1"), identity)
+
+	// encode/decode with delimiter in roomName
+	encoded = participantKey("room1|alter_room1", "identity1")
+	roomName, identity, err = parseParticipantKey(encoded)
+	require.NoError(t, err)
+	require.Equal(t, livekit.RoomName("room1|alter_room1"), roomName)
+	require.Equal(t, livekit.ParticipantIdentity("identity1"), identity)
+
+	// encode/decode with delimiter in identity
+	encoded = participantKey("room1", "identity1|alter-identity1")
+	roomName, identity, err = parseParticipantKey(encoded)
+	require.NoError(t, err)
+	require.Equal(t, livekit.RoomName("room1"), roomName)
+	require.Equal(t, livekit.ParticipantIdentity("identity1|alter-identity1"), identity)
+
+	// encode/decode with delimiter in both and multiple delimiters in both
+	encoded = participantKey("room1|alter_room1|again_room1", "identity1|alter-identity1|again-identity1")
+	roomName, identity, err = parseParticipantKey(encoded)
+	require.NoError(t, err)
+	require.Equal(t, livekit.RoomName("room1|alter_room1|again_room1"), roomName)
+	require.Equal(t, livekit.ParticipantIdentity("identity1|alter-identity1|again-identity1"), identity)
+}
