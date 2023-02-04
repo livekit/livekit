3136c787a635a950d50a1216a25969af73f8507f
boks1971 <raja.gobi@tutanota.com>
2023-02-04 12:28:28 +0530

    check for nil subTrack

--- pkg/rtc/subscriptionmanager.go
+++ pkg/rtc/subscriptionmanager.go
@@ -425,24 +425,25 @@ func (m *SubscriptionManager) subscribe(s *trackSubscription) error {
 	}
 
 	subTrack, err := track.AddSubscriber(m.params.Participant)
-	if err != nil {
-		if err == errAlreadySubscribed {
-			// ignore already subscribed error
-			return nil
-		}
+	if err != nil && err != errAlreadySubscribed {
+		// ignore already subscribed error
 		return err
 	}
-	subTrack.OnClose(func(willBeResumed bool) {
-		m.handleSubscribedTrackClose(s, willBeResumed)
-	})
-	subTrack.AddOnBind(func() {
-		s.setBound()
-		s.maybeRecordSuccess(m.params.Telemetry, m.params.Participant.ID())
-	})
-	s.setSubscribedTrack(subTrack)
+	if subTrack != nil { // could be nil if already subscribed
+		subTrack.OnClose(func(willBeResumed bool) {
+			m.handleSubscribedTrackClose(s, willBeResumed)
+		})
+		subTrack.AddOnBind(func() {
+			s.setBound()
+			s.maybeRecordSuccess(m.params.Telemetry, m.params.Participant.ID())
+		})
+		s.setSubscribedTrack(subTrack)
 
-	if subTrack.NeedsNegotiation() {
-		m.params.Participant.Negotiate(false)
+		if subTrack.NeedsNegotiation() {
+			m.params.Participant.Negotiate(false)
+		}
+
+		go m.params.OnTrackSubscribed(subTrack)
 	}
 
 	// add mark the participant as someone we've subscribed to
@@ -458,8 +459,6 @@ func (m *SubscriptionManager) subscribe(s *trackSubscription) error {
 	}
 	pTracks[s.trackID] = struct{}{}
 	m.lock.Unlock()
-
-	go m.params.OnTrackSubscribed(subTrack)
 
 	if changedCB != nil && firstSubscribe {
 		go changedCB(publisherID, true)
