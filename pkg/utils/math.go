81666094ae0e93d73b1d1ab5f9d1a1cc5367c887
shishir gowda <shishir@livekit.io>
2023-05-18 13:42:49 -0400

    update median calculation
    Signed-off-by: shishir gowda <shishir@livekit.io>

--- pkg/utils/math.go
+++ pkg/utils/math.go
@@ -7,6 +7,8 @@ func MedianFloat32(input []float32) float32 {
 	num := len(input)
 	if num == 0 {
 		return 0
+	} else if num == 1 {
+		return input[0]
 	}
 	sort.Slice(input, func(i, j int) bool {
 		return input[i] < input[j]
@@ -14,7 +16,7 @@ func MedianFloat32(input []float32) float32 {
 	if num%2 != 0 {
 		return input[num/2]
 	}
-	left := input[(num-1)/2]
+	left := input[num/2-1]
 	right := input[num/2]
 	return (left + right) / 2
 }
