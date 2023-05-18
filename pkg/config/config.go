4c6fe58aaa62404ddb0790b08daf45e06f7b5c80
paulwe <paulwe@gmail.com>
2023-05-18 12:30:14 -0700

    start signal relay sessions with the correct node

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -210,7 +210,6 @@ type SignalRelayConfig struct {
 	MinRetryInterval time.Duration `yaml:"min_retry_interval,omitempty"`
 	MaxRetryInterval time.Duration `yaml:"max_retry_interval,omitempty"`
 	StreamBufferSize int           `yaml:"stream_buffer_size,omitempty"`
-	MinVersion       int           `yaml:"min_version,omitempty"`
 }
 
 // RegionConfig lists available regions and their latitude/longitude, so the selector would prefer
