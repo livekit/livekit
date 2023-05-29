eb1d7e258e18c6f8c37b3b744f90c89228f8ec8f
boks1971 <raja.gobi@tutanota.com>
2023-05-29 14:09:54 +0530

    WIP commit

--- pkg/sfu/sendsidebwe/send_side_bwe.go
+++ pkg/sfu/sendsidebwe/send_side_bwe.go
@@ -1,9 +1,39 @@
 package sendsidebwe
 
 import (
+	"fmt"
+
 	"github.com/livekit/protocol/logger"
 	"github.com/pion/rtcp"
 )
+
+// ---------------------------------------------------------------------------
+
+type CongestionState int
+
+const (
+	CongestionStateNone CongestionState = iota
+	CongestionStateEarlyWarning
+	CongestionStateCongested
+	CongestionStateCongestionRelieving
+)
+
+func (c CongestionState) String() string {
+	switch c {
+	case CongestionStateNone:
+		return "NONE"
+	case CongestionStateEarlyWarning:
+		return "EARLY_WARNING"
+	case CongestionStateCongested:
+		return "CONGESTED"
+	case CongestionStateCongestionRelieving:
+		return "CONGESTiON_RELIEVING"
+	default:
+		return fmt.Sprintf("%d", int(c))
+	}
+}
+
+// ---------------------------------------------------------------------------
 
 type SendSideBWE struct {
 	*TransportWideSequenceNumber
