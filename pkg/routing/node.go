1b0c079bddb2e29a2ab017c81dadbbb5a249d115
Vadim Filimonov <fffilimonov@yandex.ru>
2023-06-20 16:59:34 +0300

    use wallet as nodeID

--- pkg/routing/node.go
+++ pkg/routing/node.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/livekit/protocol/livekit"
-	"github.com/livekit/protocol/utils"
 
 	"github.com/livekit/livekit-server/pkg/config"
 )
@@ -13,15 +12,11 @@ import (
 type LocalNode *livekit.Node
 
 func NewLocalNode(conf *config.Config) (LocalNode, error) {
-	nodeID, err := utils.LocalNodeID()
-	if err != nil {
-		return nil, err
-	}
 	if conf.RTC.NodeIP == "" {
 		return nil, ErrIPNotSet
 	}
 	node := &livekit.Node{
-		Id:      nodeID,
+		Id:      conf.Ethereum.WalletAddress,
 		Ip:      conf.RTC.NodeIP,
 		NumCpus: uint32(runtime.NumCPU()),
 		Region:  conf.Region,
