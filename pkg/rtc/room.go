469f1cd073b2806c463a94f3831bba5ae4aa6748
Raja Subramanian <raja.gobi@tutanota.com>
2023-07-15 12:43:05 +0530

    Minor changes to publisher bool. (#1880)
    * Minor changes to publisher bool.
    
    * address feedback

--- pkg/rtc/room.go
+++ pkg/rtc/room.go
@@ -828,7 +828,6 @@ func (r *Room) createJoinResponseLocked(participant types.LocalParticipant, iceS
 func (r *Room) onTrackPublished(participant types.LocalParticipant, track types.MediaTrack) {
 	// publish participant update, since track state is changed
 	r.broadcastParticipantState(participant, broadcastOptions{skipSource: true})
-	r.protoProxy.MarkDirty(false)
 
 	r.lock.RLock()
 	// subscribe all existing participants to this MediaTrack
@@ -888,7 +887,6 @@ func (r *Room) onTrackUpdated(p types.LocalParticipant, _ types.MediaTrack) {
 
 func (r *Room) onTrackUnpublished(p types.LocalParticipant, track types.MediaTrack) {
 	r.trackManager.RemoveTrack(track)
-	r.protoProxy.MarkDirty(false)
 	if !p.IsClosed() {
 		r.broadcastParticipantState(p, broadcastOptions{skipSource: true})
 	}
@@ -898,6 +896,7 @@ func (r *Room) onTrackUnpublished(p types.LocalParticipant, track types.MediaTra
 }
 
 func (r *Room) onParticipantUpdate(p types.LocalParticipant) {
+	r.protoProxy.MarkDirty(false)
 	// immediately notify when permissions or metadata changed
 	r.broadcastParticipantState(p, broadcastOptions{immediate: true})
 	if r.onParticipantChanged != nil {
