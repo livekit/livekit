1805530f4a29e364cd5d897317cd84776ef1df7c
David Colburn <xero73@gmail.com>
2023-10-18 00:30:09 -0700

    infinite loops

--- pkg/service/ioclient.go
+++ pkg/service/ioclient.go
@@ -19,29 +19,29 @@ func NewIOClient(io *IOInfoService) rpc.IOInfoClient {
 }
 
 func (c *ioClient) CreateEgress(ctx context.Context, req *livekit.EgressInfo, _ ...psrpc.RequestOption) (*emptypb.Empty, error) {
-	return c.CreateEgress(ctx, req)
+	return c.io.CreateEgress(ctx, req)
 }
 
 func (c *ioClient) UpdateEgress(ctx context.Context, req *livekit.EgressInfo, _ ...psrpc.RequestOption) (*emptypb.Empty, error) {
-	return c.UpdateEgress(ctx, req)
+	return c.io.UpdateEgress(ctx, req)
 }
 
 func (c *ioClient) GetEgress(ctx context.Context, req *rpc.GetEgressRequest, _ ...psrpc.RequestOption) (*livekit.EgressInfo, error) {
-	return c.GetEgress(ctx, req)
+	return c.io.GetEgress(ctx, req)
 }
 
 func (c *ioClient) ListEgress(ctx context.Context, req *livekit.ListEgressRequest, _ ...psrpc.RequestOption) (*livekit.ListEgressResponse, error) {
-	return c.ListEgress(ctx, req)
+	return c.io.ListEgress(ctx, req)
 }
 
 func (c *ioClient) GetIngressInfo(ctx context.Context, req *rpc.GetIngressInfoRequest, _ ...psrpc.RequestOption) (*rpc.GetIngressInfoResponse, error) {
-	return c.GetIngressInfo(ctx, req)
+	return c.io.GetIngressInfo(ctx, req)
 }
 
 func (c *ioClient) UpdateIngressState(ctx context.Context, req *rpc.UpdateIngressStateRequest, _ ...psrpc.RequestOption) (*emptypb.Empty, error) {
-	return c.UpdateIngressState(ctx, req)
+	return c.io.UpdateIngressState(ctx, req)
 }
 
 func (c *ioClient) UpdateEgressInfo(ctx context.Context, req *livekit.EgressInfo, _ ...psrpc.RequestOption) (*emptypb.Empty, error) {
-	return c.UpdateEgressInfo(ctx, req)
+	return c.io.UpdateEgressInfo(ctx, req)
 }
