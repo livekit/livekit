6de5e3c39a4dbbf4f6118e7496a5f25a493531e5
cnderrauber <zengjie9004@gmail.com>
2023-10-25 16:44:06 +0800

    Remove un-preferred codecs for android firefox
    Android firefox don't comply with the codec order in answer sdp and
    has problem to publish h.264, remove other codecs to fix this.

--- pkg/rtc/participant_sdp.go
+++ pkg/rtc/participant_sdp.go
@@ -180,7 +180,10 @@ func (p *ParticipantImpl) setCodecPreferencesVideoForPublisher(offer webrtc.Sess
 			}
 
 			unmatchVideo.MediaName.Formats = append(unmatchVideo.MediaName.Formats[:0], preferredCodecs...)
-			unmatchVideo.MediaName.Formats = append(unmatchVideo.MediaName.Formats, leftCodecs...)
+			// if the client don't comply with codec order in SDP answer, only keep preferred codecs to force client to use it
+			if !p.params.ClientInfo.ComplyWithCodecOrderInSDPAnswer() {
+				unmatchVideo.MediaName.Formats = append(unmatchVideo.MediaName.Formats, leftCodecs...)
+			}
 		}
 	}
 
