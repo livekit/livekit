1a40fcbcf1e5016dd7140ee18b8eed95c288b597
Tobias Ollmann <tobias.ollmann@instahelp.me>
2023-11-20 13:36:05 +0100

    fix: properly name middleware generator

--- pkg/service/basic_auth.go
+++ pkg/service/basic_auth.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-func GenMiddleware(username string, password string) (func(http.ResponseWriter, *http.Request, http.HandlerFunc) ) {
+func GenBasicAuthMiddleware(username string, password string) (func(http.ResponseWriter, *http.Request, http.HandlerFunc) ) {
 	return func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 		given_username, given_password, ok := r.BasicAuth()
 		unauthorized := func() {
