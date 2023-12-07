18d3f26de4c6618cbc0b7b406cd9544007f9220b
cnderrauber <zengjie9004@gmail.com>
2023-12-07 11:54:33 +0800

    Fix svc freeze caused by key frame id wrap around

---
+++ pkg/sfu/videolayerselector/framenumberwrapper_test.go
@@ -0,0 +1,82 @@
+package videolayerselector
+
+import (
+	"testing"
+
+	"math/rand"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/livekit/livekit-server/pkg/sfu/utils"
+	"github.com/livekit/protocol/logger"
+)
+
+func TestFrameNumberWrapper(t *testing.T) {
+
+	logger.InitFromConfig(&logger.Config{Level: "debug"}, t.Name())
+
+	fnWrap := &FrameNumberWrapper{logger: logger.GetLogger()}
+
+	fnWrapAround := utils.NewWrapAround[uint16, uint64](utils.WrapAroundParams{IsRestartAllowed: false})
+
+	firstKF := uint16(1000)
+
+	testFrameOrder := func(frame uint16, isKeyFrame bool, frame2 uint16, isKeyFrame2, expectInorder bool) {
+		frameUnwrap := fnWrapAround.Update(frame).ExtendedVal
+		wrappedFrame := uint16(fnWrap.UpdateAndGet(frameUnwrap, isKeyFrame))
+
+		// make sure wrap around always get in order frame number
+		fnWrapAround.Update(frame + (frame2-frame)/2)
+
+		frame2Unwrap := fnWrapAround.Update(frame2).ExtendedVal
+		wrappedFrame2 := uint16(fnWrap.UpdateAndGet(frame2Unwrap, isKeyFrame2))
+		// keeps order
+		require.Equal(t, expectInorder, inOrder(wrappedFrame2, wrappedFrame), "frame %d, frame2 %d, wrappedFrame %d, wrapped Frame2 %d, frameUnwrap %d, frame2Unwrap %d", frame, frame2, wrappedFrame, wrappedFrame2, frameUnwrap, frame2Unwrap)
+		// frame number diff should be the same if frame2 is not a key frame
+		if !isKeyFrame2 {
+			require.Equal(t, frame2-frame, wrappedFrame2-wrappedFrame)
+		}
+	}
+
+	secondF := getFrame(firstKF, true)
+	testFrameOrder(firstKF, true, secondF, false, true)
+
+	// non key frame keeps diff and order
+	for i := 0; i < 100; i++ {
+		// frame in order
+		firstKF = secondF
+		secondF = getFrame(firstKF, true)
+		testFrameOrder(firstKF, false, secondF, false, true)
+
+		// frame out of order
+		firstKF = secondF
+		secondF = getFrame(firstKF, false)
+		testFrameOrder(firstKF, false, secondF, false, false)
+
+		// key frame in order
+		firstKF = secondF
+		secondF = getFrame(firstKF, true)
+		testFrameOrder(firstKF, false, secondF, true, true)
+
+		// frame in order
+		firstKF = secondF
+		secondF = getFrame(firstKF, true)
+		testFrameOrder(firstKF, false, secondF, false, true)
+
+		// key frame out of order but should be in order after wrap around
+		firstKF = secondF
+		secondF = getFrame(firstKF, false)
+		testFrameOrder(firstKF, false, secondF, true, true)
+	}
+}
+
+func inOrder(a, b uint16) bool {
+	return a-b < 0x8000 || (a-b == 0x8000 && a > b)
+}
+
+func getFrame(base uint16, inorder bool) uint16 {
+	if inorder {
+		return base + uint16(rand.Intn(0x8000))
+	}
+	return base + uint16(rand.Intn(0x8000)) + 0x8000
+}
