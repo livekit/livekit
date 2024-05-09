b54acb0bad6447623996993609974c1a6c9f2a0f
Paul Wells <paulwe@gmail.com>
2024-05-09 00:06:41 -0700

    ensure room is running before attempting to delete (#2705)
    * ignore api errors when deleting room
    
    * start room before deletion
    
    * delete from db first
    
    * load

--- pkg/service/roomservice.go
+++ pkg/service/roomservice.go
@@ -29,7 +29,6 @@ import (
 	"github.com/livekit/livekit-server/pkg/rtc"
 	"github.com/livekit/protocol/livekit"
 	"github.com/livekit/protocol/rpc"
-	"github.com/livekit/psrpc"
 )
 
 // A rooms service that supports a single node
@@ -94,16 +93,11 @@ func (s *RoomService) CreateRoom(ctx context.Context, req *livekit.CreateRoomReq
 		return nil, err
 	}
 
-	// actually start the room on an RTC node, to ensure metadata & empty timeout functionality
-	res, err := s.router.StartParticipantSignal(ctx,
-		livekit.RoomName(req.Name),
-		routing.ParticipantInit{},
-	)
+	done, err := s.startRoom(ctx, livekit.RoomName(req.Name))
 	if err != nil {
 		return nil, err
 	}
-	defer res.RequestSink.Close()
-	defer res.ResponseSource.Close()
+	defer done()
 
 	if created {
 		go s.agentClient.LaunchJob(ctx, &agent.JobDescription{
@@ -158,9 +152,20 @@ func (s *RoomService) DeleteRoom(ctx context.Context, req *livekit.DeleteRoomReq
 		return nil, twirpAuthError(err)
 	}
 
-	_, err := s.roomClient.DeleteRoom(ctx, s.topicFormatter.RoomTopic(ctx, livekit.RoomName(req.Room)), req)
-	if !errors.Is(err, psrpc.ErrNoResponse) {
-		return &livekit.DeleteRoomResponse{}, err
+	_, _, err := s.roomStore.LoadRoom(ctx, livekit.RoomName(req.Room), false)
+	if err != nil {
+		return nil, err
+	}
+
+	done, err := s.startRoom(ctx, livekit.RoomName(req.Room))
+	if err != nil {
+		return nil, err
+	}
+	defer done()
+
+	_, err = s.roomClient.DeleteRoom(ctx, s.topicFormatter.RoomTopic(ctx, livekit.RoomName(req.Room)), req)
+	if err != nil {
+		return nil, err
 	}
 
 	err = s.roomStore.DeleteRoom(ctx, livekit.RoomName(req.Room))
@@ -324,4 +329,16 @@ func (s *RoomService) confirmExecution(ctx context.Context, f func() error) erro
 		retry.MaxDelay(s.apiConf.MaxCheckInterval),
 		retry.DelayType(retry.BackOffDelay),
 	)
+}
+
+// startRoom starts the room on an RTC node, to ensure metadata & empty timeout functionality
+func (s *RoomService) startRoom(ctx context.Context, roomName livekit.RoomName) (func(), error) {
+	res, err := s.router.StartParticipantSignal(ctx, roomName, routing.ParticipantInit{})
+	if err != nil {
+		return nil, err
+	}
+	return func() {
+		res.RequestSink.Close()
+		res.ResponseSource.Close()
+	}, nil
 }
