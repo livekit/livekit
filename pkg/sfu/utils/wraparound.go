7088895ad8df5d3814c9eda2717965f5c225edf1
boks1971 <raja.gobi@tutanota.com>
2024-07-29 10:39:51 +0530

    test

--- pkg/sfu/utils/wraparound.go
+++ pkg/sfu/utils/wraparound.go
@@ -112,7 +112,7 @@ func (w *WrapAround[T, ET]) Update(val T) (result WrapAroundUpdateResult[ET]) {
 }
 
 func (w *WrapAround[T, ET]) UndoUpdate(result WrapAroundUpdateResult[ET]) {
-	if !w.initialized || result.PreExtendedHighest <= result.ExtendedVal {
+	if !w.initialized || result.PreExtendedHighest >= result.ExtendedVal {
 		return
 	}
 
