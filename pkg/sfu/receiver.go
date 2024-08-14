75a591b203aead43a76e9f264e9f6c70fede6d4d
cnderrauber <zengjie9004@gmail.com>
2024-08-14 10:12:09 +0800

    Add tracksubscribed event on downtrack added
    will remove this once the remote node has tracksubscribed
    event sent.

--- pkg/sfu/receiver.go
+++ pkg/sfu/receiver.go
@@ -120,8 +120,10 @@ type WebRTCReceiver struct {
 
 	connectionStats *connectionquality.ConnectionStats
 
-	onStatsUpdate    func(w *WebRTCReceiver, stat *livekit.AnalyticsStat)
-	onMaxLayerChange func(maxLayer int32)
+	onStatsUpdate        func(w *WebRTCReceiver, stat *livekit.AnalyticsStat)
+	onMaxLayerChange     func(maxLayer int32)
+	downTrackEverAdded   atomic.Bool
+	onDownTrackEverAdded func()
 
 	primaryReceiver atomic.Pointer[RedPrimaryReceiver]
 	redReceiver     atomic.Pointer[RedReceiver]
@@ -171,6 +173,13 @@ func WithLoadBalanceThreshold(downTracks int) ReceiverOpts {
 func WithForwardStats(forwardStats *ForwardStats) ReceiverOpts {
 	return func(w *WebRTCReceiver) *WebRTCReceiver {
 		w.forwardStats = forwardStats
+		return w
+	}
+}
+
+func WithEverHasDownTrackAdded(f func()) ReceiverOpts {
+	return func(w *WebRTCReceiver) *WebRTCReceiver {
+		w.onDownTrackEverAdded = f
 		return w
 	}
 }
@@ -411,7 +420,14 @@ func (w *WebRTCReceiver) AddDownTrack(track TrackSender) error {
 
 	w.downTrackSpreader.Store(track)
 	w.logger.Debugw("downtrack added", "subscriberID", track.SubscriberID())
+	w.handleDowntrackAdded()
 	return nil
+}
+
+func (w *WebRTCReceiver) handleDowntrackAdded() {
+	if !w.downTrackEverAdded.Swap(true) && w.onDownTrackEverAdded != nil {
+		w.onDownTrackEverAdded()
+	}
 }
 
 func (w *WebRTCReceiver) notifyMaxExpectedLayer(layer int32) {
@@ -776,6 +792,7 @@ func (w *WebRTCReceiver) GetPrimaryReceiverForRed() TrackReceiver {
 			w.bufferMu.Lock()
 			w.redPktWriter = pr.ForwardRTP
 			w.bufferMu.Unlock()
+			w.handleDowntrackAdded()
 		}
 	}
 	return w.primaryReceiver.Load()
@@ -795,6 +812,7 @@ func (w *WebRTCReceiver) GetRedReceiver() TrackReceiver {
 			w.bufferMu.Lock()
 			w.redPktWriter = pr.ForwardRTP
 			w.bufferMu.Unlock()
+			w.handleDowntrackAdded()
 		}
 	}
 	return w.redReceiver.Load()
