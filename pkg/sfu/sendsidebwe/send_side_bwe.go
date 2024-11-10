e52755518bac36d7416a8637dad2e85eb27d5eaa
boks1971 <raja.gobi@tutanota.com>
2024-11-10 20:21:48 +0530

    comments

--- pkg/sfu/sendsidebwe/send_side_bwe.go
+++ pkg/sfu/sendsidebwe/send_side_bwe.go
@@ -6,6 +6,34 @@ import (
 	"github.com/livekit/protocol/logger"
 )
 
+//
+// Based on a simplified/modified version of JitterPath paper
+// (https://homepage.iis.sinica.edu.tw/papers/lcs/2114-F.pdf)
+//
+// TWCC feedback is uesed to calcualte delta one-way-delay.
+// It is accumulated/propagated to determine in which region
+// groups of packets are operating in.
+//
+// In simplified terms,
+//   o JQR (Join Queuing Region) is when channel is congested.
+//   o DQR (Disjoint Queuing Region) is when channel is not.
+//
+// Packets are grouped and thresholds applied to smooth over
+// small variations. For example, in the paper,
+//    if propagated_queuing_delay + delta_one_way_delay > 0 {
+//       possibly_operating_in_jqr
+//    }
+// But, in this implementation it is checked at packet group level,
+// i. e. using queuing delay and aggreated delta one-way-delay of
+// the group and a minimum value threshold is applied before declaring
+// that a group is in JQR.
+//
+// There is also hysteresis to make transisitons smoother, i.e. if the
+// metric is above a certain threshold, it is JQR and it is DQR only if it
+// is below a certain value and the gap in between those two thresholds
+// are treated as interdeterminate groups.
+//
+
 // ---------------------------------------------------------------------------
 
 type CongestionState int
