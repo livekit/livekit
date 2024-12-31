bfbf9ca2f40d89d2a43f5a8f6815d0599f9d58e0
Rajiv <rajivgg91@gmail.com>
2024-12-31 00:23:51 +0000

    depends on new protocol release with sessionEnabled flag

--- pkg/service/ingress.go
+++ pkg/service/ingress.go
@@ -261,6 +261,10 @@ func updateInfoUsingRequest(req *livekit.UpdateIngressRequest, info *livekit.Ing
 		info.Video = req.Video
 	}
 
+	if req.SessionEnabled != nil {
+		info.SessionEnabled = req.SessionEnabled
+	}
+
 	if err := ingress.ValidateForSerialization(info); err != nil {
 		return err
 	}
