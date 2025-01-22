004fb67c6f4ce8bbd5aad220a64326da2fbca091
boks1971 <raja.gobi@tutanota.com>
2025-01-22 14:10:51 +0530

    switch to compare semantics

--- pkg/rtc/room.go
+++ pkg/rtc/room.go
@@ -1392,7 +1392,7 @@ func (r *Room) pushAndDequeueUpdates(
 			}
 		} else {
 			// different participant sessions
-			if livekit.ParticipantID(existing.pi.Sid) != ResolveNewerParticipantID(existing.pi, pi) {
+			if CompareParticipant(existing.pi, pi) < 0 {
 				// existing is older, synthesize a DISCONNECT for older and
 				// send immediately along with newer session to signal switch
 				shouldSend = true
@@ -1408,7 +1408,7 @@ func (r *Room) pushAndDequeueUpdates(
 		ep := r.GetParticipant(identity)
 		if ep != nil {
 			epi := ep.ToProto()
-			if livekit.ParticipantID(epi.Sid) == ResolveNewerParticipantID(epi, pi) {
+			if CompareParticipant(epi, pi) >= 0 {
 				// older session update, newer session has already become active, so nothing to do
 				return nil
 			}
@@ -1815,29 +1815,34 @@ func IsParticipantExemptFromTrackPermissionsRestrictions(p types.LocalParticipan
 	return p.IsRecorder()
 }
 
-func ResolveNewerParticipantID(pi1 *livekit.ParticipantInfo, pi2 *livekit.ParticipantInfo) livekit.ParticipantID {
+func CompareParticipant(pi1 *livekit.ParticipantInfo, pi2 *livekit.ParticipantInfo) int {
 	if pi1.JoinedAt != pi2.JoinedAt {
 		if pi1.JoinedAt < pi2.JoinedAt {
-			return livekit.ParticipantID(pi2.Sid)
+			return -1
 		} else {
-			return livekit.ParticipantID(pi1.Sid)
+			return 1
 		}
 	}
 
 	if pi1.JoinedAtMs != 0 && pi2.JoinedAtMs != 0 && pi1.JoinedAtMs != pi2.JoinedAtMs {
 		if pi1.JoinedAtMs < pi2.JoinedAtMs {
-			return livekit.ParticipantID(pi2.Sid)
+			return -1
 		} else {
-			return livekit.ParticipantID(pi1.Sid)
+			return 1
 		}
 	}
 
 	// all join times being equal, it is not possible to really know which one is newer,
 	// pick the higher pID to be consistent
-	if pi1.Sid > pi2.Sid {
-		return livekit.ParticipantID(pi1.Sid)
+	if pi1.Sid != pi2.Sid {
+		if pi1.Sid < pi2.Sid {
+			return -1
+		} else {
+			return 1
+		}
 	}
-	return livekit.ParticipantID(pi2.Sid)
+
+	return 0
 }
 
 func connectionDetailsFields(infos []*types.ICEConnectionInfo) []interface{} {
