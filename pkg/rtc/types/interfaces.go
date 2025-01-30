cd7351e54ef4fe89cee8717781ba37ca0e86c562
Paul Wells <paulwe@gmail.com>
2025-01-30 05:42:34 -0800

    move ConnectedAt to Participant interface

--- pkg/rtc/types/interfaces.go
+++ pkg/rtc/types/interfaces.go
@@ -264,6 +264,7 @@ type Participant interface {
 	ID() livekit.ParticipantID
 	Identity() livekit.ParticipantIdentity
 	State() livekit.ParticipantInfo_State
+	ConnectedAt() time.Time
 	CloseReason() ParticipantCloseReason
 	Kind() livekit.ParticipantInfo_Kind
 	IsRecorder() bool
@@ -324,7 +325,6 @@ type LocalParticipant interface {
 	ProtocolVersion() ProtocolVersion
 	SupportsSyncStreamID() bool
 	SupportsTransceiverReuse() bool
-	ConnectedAt() time.Time
 	IsClosed() bool
 	IsReady() bool
 	IsDisconnected() bool
