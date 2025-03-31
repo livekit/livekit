d4a44843405e9c71a359bc9fd1dd85e6784a6f96
boks1971 <raja.gobi@tutanota.com>
2025-03-31 21:08:21 +0530

    Audio uses signal SignalCid and SdpCid.
    This is a bit annoying and painful. Audio does not have the information
    in the track info (because it does not use Codecs, it should probably at
    some point to make it consistent).

--- pkg/rtc/mediatrack.go
+++ pkg/rtc/mediatrack.go
@@ -60,6 +60,8 @@ type MediaTrack struct {
 }
 
 type MediaTrackParams struct {
+	SignalCid             string
+	SdpCid                string
 	ParticipantID         livekit.ParticipantID
 	ParticipantIdentity   livekit.ParticipantIdentity
 	ParticipantVersion    uint32
@@ -175,6 +177,10 @@ func (t *MediaTrack) NotifySubscriberNodeMaxQuality(nodeID livekit.NodeID, quali
 }
 
 func (t *MediaTrack) HasSignalCid(cid string) bool {
+	if t.params.SignalCid == cid {
+		return true
+	}
+
 	for _, c := range t.MediaTrackReceiver.TrackInfo().Codecs {
 		if c.SignalCid == cid {
 			return true
@@ -194,6 +200,10 @@ func (t *MediaTrack) SdpCids() []string {
 }
 
 func (t *MediaTrack) HasSdpCid(cid string) bool {
+	if t.params.SdpCid == cid {
+		return true
+	}
+
 	for _, c := range t.MediaTrackReceiver.TrackInfo().Codecs {
 		if c.SdpCid == cid {
 			return true
