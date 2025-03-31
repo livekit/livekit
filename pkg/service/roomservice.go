381a1dc9de3d1e4382f8561f111a629389a74a73
cnderrauber <zengjie9004@gmail.com>
2025-03-31 16:10:26 +0800

    Enforce destination room name in `ForwardParticipant`
    Need to update the protocol description about the api

--- pkg/service/roomservice.go
+++ pkg/service/roomservice.go
@@ -321,7 +321,7 @@ func (s *RoomService) ForwardParticipant(ctx context.Context, req *livekit.Forwa
 
 	roomName := livekit.RoomName(req.Room)
 	AppendLogFields(ctx, "room", roomName, "participant", req.Identity)
-	if err := EnsureAdminPermission(ctx, roomName); err != nil {
+	if err := EnsureAdminPermission(ctx, livekit.RoomName(fmt.Sprintf("%s|%s", req.Room, req.DestinationRoom))); err != nil {
 		return nil, twirpAuthError(err)
 	}
 
