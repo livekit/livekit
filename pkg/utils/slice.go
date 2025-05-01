cdeb9ba054750d99ed356dcf7d9e83b2a0962db4
boks1971 <raja.gobi@tutanota.com>
2025-05-01 13:38:37 +0530

    use slices.Compact

--- pkg/utils/slice.go
+++ pkg/utils/slice.go
@@ -6,19 +6,6 @@ import (
 )
 
 func DedupeSlice[T cmp.Ordered](s []T) []T {
-	if len(s) < 2 {
-		return s
-	}
 	slices.Sort(s)
-
-	o := 1
-	for i := 1; i < len(s); i++ {
-		if s[i] == s[i-1] {
-			continue
-		}
-		s[o] = s[i]
-		o++
-	}
-
-	return s[:o]
+	return slices.Compact(s)
 }
