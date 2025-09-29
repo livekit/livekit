97a13c80b60c4eab8e46ea3f0235f73d76537a6e
Paul Wells <paulwe@gmail.com>
2025-09-29 02:50:26 -0700

    tidy

--- pkg/telemetry/statsworker_test.go
+++ pkg/telemetry/statsworker_test.go
@@ -11,9 +11,9 @@ func TestStatsWorker(t *testing.T) {
 		var g0, g1 ReferenceGuard
 		w := newStatsWorker(t.Context(), nil, "", "", "", "", &g0)
 		require.False(t, w.Closed(&g1))
-		w.Close(&g0)
+		require.False(t, w.Close(&g0))
 		require.False(t, w.Closed(&g1))
-		w.Close(&g1)
+		require.True(t, w.Close(&g1))
 		require.True(t, w.Closed(&g1))
 	})
 }
