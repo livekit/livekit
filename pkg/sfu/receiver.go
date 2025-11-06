3aaa22d44b4a57ff560536c4777281ff6c57f816
boks1971 <raja.gobi@tutanota.com>
2025-11-06 12:49:57 +0530

    Log write count atomic.

--- pkg/sfu/receiver.go
+++ pkg/sfu/receiver.go
@@ -835,7 +835,7 @@ func (w *WebRTCReceiver) forwardRTP(layer int32, buff *buffer.Buffer) {
 				w.logger.Infow(
 					"high forwarding latency",
 					"latency", latency,
-					"writeCount", writeCount,
+					"writeCount", writeCount.Load(),
 					"queuingLatency", dequeuedAt-pkt.Arrival,
 					"isOutOfOrder", pkt.IsOutOfOrder,
 				)
