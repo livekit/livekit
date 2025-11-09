4ea2bb83fc971f4ba78754a627e13a2b58f63c22
boks1971 <raja.gobi@tutanota.com>
2025-11-09 17:08:42 +0530

    one more typo

--- pkg/telemetry/prometheus/packets.go
+++ pkg/telemetry/prometheus/packets.go
@@ -188,7 +188,7 @@ func initPacketStats(nodeID string, nodeType livekit.NodeType) {
 			500 * 1000,
 			1 * 1000 * 1000,
 			2 * 1000 * 1000,
-			3 * 3000 * 1000,
+			3 * 1000 * 1000,
 			5 * 1000 * 1000,
 			10 * 1000 * 1000,
 			20 * 1000 * 1000,
