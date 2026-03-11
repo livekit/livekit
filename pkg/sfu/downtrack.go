3b57e6e90e96a4ba9da1809aab8c77e7489681cd
elizabeth-muirhead-simplisafe <elizabeth.muirhead@simplisafe.com>
2026-03-11 15:18:45 -0400

    fix: minimal downtrack changes

--- pkg/sfu/downtrack.go
+++ pkg/sfu/downtrack.go
@@ -815,11 +815,12 @@ func (d *DownTrack) SetReceiver(r TrackReceiver) {
 // Sets RTP header extensions for this track
 func (d *DownTrack) setRTPHeaderExtensions() {
 	sal := d.getStreamAllocatorListener()
-	if sal == nil {
-		return
+	isBWEEnabled := false
+	bweType := bwe.BWETypeNone
+	if sal != nil {
+		isBWEEnabled = sal.IsBWEEnabled(d)
+		bweType = sal.BWEType()
 	}
-	isBWEEnabled := sal.IsBWEEnabled(d)
-	bweType := sal.BWEType()
 
 	tr := d.transceiver.Load()
 	if tr == nil {
