31d433ef976dfa5bc2b84b4007a0f6db3a1320fc
boks1971 <raja.gobi@tutanota.com>
2026-05-14 18:42:01 +0530

    revert debug

--- pkg/rtc/transport.go
+++ pkg/rtc/transport.go
@@ -1434,7 +1434,7 @@ func (t *PCTransport) GetICEConnectionType() types.ICEConnectionType {
 func (t *PCTransport) WriteRTCP(pkts []rtcp.Packet) error {
 	// TODO-CLEANUP-PACKET-SIZE: remove after checking for large packets
 	raw, _ := rtcp.Marshal(pkts)
-	if len(raw) > 00 {
+	if len(raw) > 1400 {
 		t.params.Logger.Infow("large RTCP packet send", "size", len(raw), "numPkts", len(pkts), "pkts", pkts)
 	}
 	return t.pc.WriteRTCP(pkts)
