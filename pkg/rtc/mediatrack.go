3071bc3bba90ac8e2be69781c88dd6d8b7f7bdb3
boks1971 <raja.gobi@tutanota.com>
2026-05-20 23:47:52 +0530

    Clean up packet size logging
    Reverting
    - https://github.com/livekit/livekit/pull/4521
    - https://github.com/livekit/livekit/pull/4525
    
    There are TWCC feedback packets that are larger than MTU. Seems to
    happen under a couple of conditions
    1. Bad client data, i. e. severely out-of-order packets, bad sequence
       numbers, etc.
    2. On an ICE restart - this is rare, but it seemed to be flaky network
       with some packets arriving and some not and causing a lot of gaps.
    
    Either case, not much to do. If fargmentation/re-assembly back to
    publisher works, the feedback will make it through. If not, feedbacks
    will be missed and clients have to work with some missing data which is
    not unexpected and the protocol is designed to handle.
    
    However, filed pion/interceptor issue just in case - https://github.com/pion/interceptor/issues/416

--- pkg/rtc/mediatrack.go
+++ pkg/rtc/mediatrack.go
@@ -303,7 +303,6 @@ func (t *MediaTrack) AddReceiver(receiver *webrtc.RTPReceiver, track sfu.TrackRe
 
 	var lastRR uint32
 	rtcpReader.OnPacket(func(bytes []byte) {
-		isLargePacket := len(bytes) > 1400
 		pkts, err := rtcp.Unmarshal(bytes)
 		if err != nil {
 			t.params.Logger.Errorw("could not unmarshal RTCP", err, "size", len(bytes))
@@ -313,13 +312,7 @@ func (t *MediaTrack) AddReceiver(receiver *webrtc.RTPReceiver, track sfu.TrackRe
 		for _, pkt := range pkts {
 			switch pkt := pkt.(type) {
 			case *rtcp.SourceDescription:
-				if isLargePacket {
-					t.params.Logger.Infow("large RTCP packet received with SDES", "size", len(bytes))
-				}
 			case *rtcp.SenderReport:
-				if isLargePacket {
-					t.params.Logger.Infow("large RTCP packet received with sender report", "size", len(bytes), "SSRC", pkt.SSRC)
-				}
 				if pkt.SSRC == uint32(track.SSRC()) {
 					buff.SetSenderReportData(&livekit.RTCPSenderReportState{
 						RtpTimestamp: pkt.RTPTime,
@@ -330,9 +323,6 @@ func (t *MediaTrack) AddReceiver(receiver *webrtc.RTPReceiver, track sfu.TrackRe
 					})
 				}
 			case *rtcp.ExtendedReport:
-				if isLargePacket {
-					t.params.Logger.Infow("large RTCP packet received with extendedreport", "size", len(bytes))
-				}
 			rttFromXR:
 				for _, report := range pkt.Reports {
 					if rr, ok := report.(*rtcp.DLRRReportBlock); ok {
